7691be41c3bdec088eb8f22ed32e8f815a7c3b8a
MoMeak9 <1308994506@qq.com>
2022-02-27 22:12:41 +0800

    Leetcode 刷题 day15

--- 搜索/401. 二进制手表.js
+++ 搜索/401. 二进制手表.js
@@ -1,3 +1,19 @@
 /**
  * Created by Yihui_Shi on 2022/2/25 13:06
  */
+/**
+ * @description 枚举所有情况
+ * @param {number} turnedOn
+ * @return {string[]}
+ */
+var readBinaryWatch = function(turnedOn) {
+    const ans = [];
+    for (let h = 0; h < 12; ++h) {
+        for (let m = 0; m < 60; ++m) {
+            if (h.toString(2).split('0').join('').length + m.toString(2).split('0').join('').length === turnedOn) {
+                ans.push(h + ":" + (m < 10 ? "0" : "") + m);
+            }
+        }
+    }
+    return ans;
+};
